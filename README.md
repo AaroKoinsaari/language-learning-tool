1b851bfef8e75055b8d6bb65124035b79f3b3d03
Aaro Koinsaari <aaro.koinsaari@proton.me>
2023-07-31 13:06:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Language Learning Tool Program
+
+## Description
+
+A simple console program for practicing French-English translations. The program provides a quiz-like interface where users are prompted to translate words from one language to the other.
+
+## Features
+
+- **Translation Challenges**: The program presents English words and asks for the corresponding French translations.
+- **Randomized Questions**: Words are presented in random order to enhance learning.
+
+## Future Improvements
+
+- **Addition of Sentences**: Planning to add more complex translation exercises that involve full sentences instead of individual words, providing a more challenging and realistic language learning experience.
+- **English-to-French or French-to-English Options**: Adding the ability for users to choose the direction of translation, allowing for a more versatile and customized learning path.
+- **Tracking User Progress**: Implementing features to track and visualize the user's progress over time, including accuracy rates and completed exercises, to motivate continuous improvement.
+- **Enhanced Error Handling and User Input Validation**: Improving the robustness of the program by handling unexpected inputs and errors better, making the user experience smoother and less prone to issues.
+- **Addition of Pronunciation Audio for Words and Sentences**: Adding an audio pronunciation for each word or sentence, allowing users to hear the correct pronunciation and enhance their listening and speaking skills.
+- **Support For Other Languages**: Expanding the program to support other languages beyond just French, making it a useful tool for learners of various languages.
+- **Addition of a GUI**: Developing a graphical user interface to replace the console-based interaction, improving usability and visual appeal, and making the program accessible to a broader audience.
+
+## Requirements
+
+- Python 3.x
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/aarokoinsaari/language-learning-tool.git
+
+2. Navigate to the directory:
+
+    cd language-learning-tool
+
+## Usage
+
+1. Run the program:
+
+    python3 src/main.py
+
+## Contact
+
+- Email: aaro.koinsaari@proton.me
+- GitHub: @aarokoinsaari
+- LinkedIn: @aarokoinsaari
+- Twitter: @aarokoinsaari
---
+++ src/main.py
@@ -0,0 +1,87 @@
+"""
+Language Tool Program
+Author: Aaro Koinsaari
+Date: 2023-07-31
+Version: 1.0.0
+
+Description:
+A simple console program for practicing French-English translations.
+The program provides a quiz-like interface where users are prompted to translate words from one 
+language to the other.
+Currently quizzes are only from English to French and the user progress is not tracked.
+
+Future Improvements:
+- Addition of sentences
+- Option to choose from English-to-French or French-to-English
+- Tracking user progress
+- Enhanced error handling and user input validation
+- Enable user to add new words (or sentences) to the dictionary
+- Addition of pronounciation audio as the word or sentence is presented
+- Support for some other languages
+- Addition of a GUI
+"""
+
+
+import csv
+import random
+
+
+def load_dictionary(file_path):
+    """
+    Loads a dictionary of English-xx word pairs from a csv-file.
+
+    Args:
+        file_path (str): The path to the csv-file.
+
+    Returns:
+        list: A list of tuples containing English and the other language word pairs.
+    """
+    dictionary = []
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        next(reader) # Skip the header
+
+        # Go through and add each word and its translation to the dictionary list
+        for row in reader:
+            dictionary.append((row[0], row[1]))
+
+    return dictionary
+
+
+def main():
+    """
+    Loads the dictionary file, shuffles its content, and
+    prompts the user to translate the words. Exit is handled
+    by typing 'q'.
+    """    
+
+    print("""
+    Welcome to the Simple Language Learning Game!
+    ------------------------------------------
+    This console program will test your knowledge of word translations.
+    You'll be given English words, and your task is to translate them into French.
+
+    To start a new word challenge, just press Enter.
+    If you want to exit the program at any time, write 'q' and press Enter.
+
+    Bonne chance!
+
+    """)
+    
+    dictionary = load_dictionary("../data/dictionary_french.csv")
+    random.shuffle(dictionary)
+
+    for english, french in dictionary:
+        user_input = input(f"Write in French: {english}\n")
+        if user_input.lower().strip() == 'q':
+            print("Exiting the game!")
+            return
+        elif user_input.lower().strip() == french.lower():
+            print("Correct!\n")
+        else:
+            print(f"Wrong! The correct translation is {french}\n")
+
+    print("Game over!")
+
+if __name__ == '__main__':
+    main()
