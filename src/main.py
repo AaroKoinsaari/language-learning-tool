8524849fe9de072040702af5b18032929a64e5ba
Aaro Koinsaari <aaro.koinsaari@proton.me>
2023-08-12 19:06:47 +0300

    feat: Extend game logic to support sentence-level translations
    - Enhanced data structure to accommodate multiple correct translations for both words and sentences.
    - Updated game logic to handle multiple translation possibilities.
    - Improved CSV loading mechanism to handle varied sentence structures.

--- src/main.py
+++ src/main.py
@@ -1,23 +1,22 @@
 """
 Simple Language Learning Tool
 Author: Aaro Koinsaari
-Date: 2023-08-04
-Version: 1.2.0
+Date: 2023-08-12
+Version: 1.3.0
 
 Description:
-A simple console program for practicing French-English translations.
-The program provides a quiz-like interface where users are prompted to translate words from one 
-language to the other while tracking user progress by keeping track of failed attempts for each
-word. Usage for this tracking is yet to be developed. User can also add prompted words to his/her
-own dictionary. Usage for this too is yet to be developed.
+A console-based program for practicing French-English translations with support for both single words and sentences.
+The program offers a quiz-like interface, prompting users to translate from one language to the other.
+It tracks user progress by recording failed attempts for each word or sentence. No usage for tracking yet.
+Users can add words or sentences to their personal dictionary for further practice. No usage for that yet.
 
 Future Improvements:
-- Addition of sentences
-- Enhanced tracking of user progress
-- Enhanced error handling and user input validation
-- Addition of pronunciation audio as the word or sentence is presented
-- Support for some other languages
-- Addition of a GUI
+- Usage for personal dictionary.
+- Enhanced tracking of user progress with analysis.
+- Enhanced error handling and user input validation.
+- Introduction of pronunciation audio as the word or sentence is presented.
+- Expansion to support additional languages.
+- Development of a Graphical User Interface (GUI).
 """
 
 import csv
@@ -27,13 +26,15 @@ import os
 
 def load_dictionary(file_path):
     """
-    Loads a dictionary of English-xx word pairs from a csv-file.
+    Loads a dictionary of English-xx word pairs from a CSV file, accommodating multiple translations.
 
     Args:
-        file_path (str): The path to the csv-file.
+        file_path (str): The path to the CSV file.
 
     Returns:
-        list: A list of tuples containing English and the other language word pairs.
+        list: A list of tuples. Each tuple contains:
+            - A list of English phrases/sentences.
+            - A list of their corresponding translations in the target language.
     """
 
     # File not found
@@ -47,9 +48,11 @@ def load_dictionary(file_path):
             reader = csv.reader(csvfile, delimiter=';')
             next(reader)  # Skip the header
 
-            # Go through and add each word and its translation to the dictionary list
+            # Go through and add each word/sentence and its translations to the dictionary list
             for row in reader:
-                dictionary.append((row[0], row[1]))
+                english_items = row[0].split(',')
+                translations = row[1].split(',')
+                dictionary.append((english_items, translations))
 
     except Exception as e:
         print(f"An error has occurred while reading the file: {e}")
@@ -58,6 +61,7 @@ def load_dictionary(file_path):
     return dictionary
 
 
+# TODO: Delete this and implement it to main function.
 def select_translation_direction():
     """
     Prompts the user to choose the direction of translation.
@@ -81,27 +85,7 @@ def select_translation_direction():
     return choice
 
 
-def get_prompt_answer(choice, source, target):
-    """
-    Determines the prompt and answer based on the user's choice of translation direction.
-
-    Args:
-        choice (str): A string representing the user's choice of translation direction.
-                      '1' for source to target language,
-                      '2' for target language to source.
-        source (str): The word in the source language.
-        target (str): The word in the target language.
-
-    Returns:
-        tuple: A tuple containing the prompt and answer for the translation.
-    """
-
-    if choice == '1':
-        return source, target
-    else:
-        return target, source
-
-
+# TODO: Fix the double quote when saving pairs that have multiple correct answers
 def save_word(username, user_input, word):
     """
     Saves a user-specified word and its corresponding translation into a CSV file.
@@ -196,11 +180,14 @@ def create_user_files(username):
 def play_game(username, dictionary, choice):
     """
     Plays the translation game based on the provided dictionary and user's choice of translation direction.
-    Keeps track of incorrect attempts and allows the user to save words to their dictionary.
+    The game supports multiple correct translations for both single words and sentences.
+    It also tracks incorrect attempts and allows the user to save words or sentences to their personal dictionary.
 
     Args:
         username (str): Player's username.
-        dictionary (list): A list of tuples containing word pairs in English and the target language.
+        dictionary (list): A list of tuples. Each tuple contains:
+            - A list of English phrases/sentences.
+            - A list of their corresponding translations in the target language.
         choice (str): A string representing the user's choice of translation direction.
                       '1' for English to target language,
                       '2' for target language to English.
@@ -210,39 +197,88 @@ def play_game(username, dictionary, choice):
               the function prints an exit message and returns.
     """
 
+    def get_prompt_answer(translation_direction, english_phrases, target_translations):
+        """
+        Determines the translation prompt and the set of correct answers based on the user's choice of
+        translation direction and the available translations in the dictionary.
+
+        Args:
+            translation_direction (str): User's choice of translation direction.
+            english_phrases (list): List of English phrases/sentences.
+            target_translations (list): List of translations in the target language.
+
+        Returns:
+            tuple: A tuple containing the translation prompt and a list of correct answers.
+        """
+
+        if translation_direction == '1':  # English to French
+            translation_prompt = random.choice(english_phrases)
+            correct_answers = target_translations
+        else:  # French to English
+            translation_prompt = random.choice(target_translations)
+            correct_answers = english_phrases
+        return translation_prompt, correct_answers
+
     # Iterate through the dictionary until the exit signal is given
-    for english, french in dictionary:
-        prompt, answer = get_prompt_answer(choice, english, french)  # Determine the translation direction
+    for english_items, translations in dictionary:
+        prompt, answers = get_prompt_answer(choice, english_items,
+                                            translations)  # Determine the translation direction
 
         attempts = 3  # Adjust the attempts for right answer
 
         # Keep the game going in a loop until the quit signal is given
         while attempts > 0:
-            user_input = input(f"Write the translation: {prompt}\n")
+            print(f"Translate: {prompt}")
+            user_input = input("Your translation: ").rstrip("...").strip()  # Remove "..." and trim spaces
 
             # Quit signal
             if user_input.lower().strip() == 'q':
                 print("Exiting the game!")
                 return
 
-            # Save word in own dictionary
+            # Save the prompted word in the personal dictionary
             elif user_input.lower().strip() == 's':
-                save_word(username, prompt, answer)
+                save_word(username, prompt, ', '.join(answers))  # Save all possible answers
                 print("Word and its translation saved in dictionary!\n")
+                continue
 
-            # Correct answer
-            elif user_input.lower().strip() == answer.lower():
-                print("Correct!\n")
-                break
+            # User wants to add a new word to the personal dictionary
+            elif user_input.lower().strip() == 'add word':
+                english_word = input("Enter the English word: ")
+                french_word = input("Enter the French word: ")
+                save_word(username, english_word, french_word)
+                print("Word added to your personal dictionary!\n")
+                continue
 
-            # Wrong answer
+            # TODO: Add sentences to personal dictionary with multiple values
+            # # User wants to add a new sentence to the personal dictionary
+            # elif user_input.lower().strip() == 'add word':
+            # english_sentence = input("Enter the English sentence: ")
+            # french_sentence = input("Enter the French sentence: ")
+            # save_word(username, english_sentence, french_sentence)
+            # print("Sentence added to your personal dictionary!\n")
+            # continue
+
             else:
-                attempts -= 1
-                add_incorrect_word(answer, username)  # Collect failed words to monitor user progress
-                if attempts > 0:
-                    print(f"Wrong! You have {attempts} attempts left.\n")
-                else:  # No attempts left
-                    print(f"Wrong! The correct translation is {answer}\n")
+                # Normalize the user's input by converting to lowercase, removing trailing ellipses, and trimming spaces
+                normalized_user_input = user_input.lower().rstrip("...").strip()
+
+                # Normalize all the correct answers
+                normalized_answers = [ans.lower().rstrip("...").strip() for ans in answers]
+
+                # Check the normalized versions to determine if the answer is correct
+                if normalized_user_input in normalized_answers:
+                    print("Correct!\n")
+                    break
+
+                # Wrong answer
+                else:
+                    attempts -= 1
+                    add_incorrect_word(prompt, username)  # Collect failed words to monitor user progress
+                    if attempts > 0:
+                        print(f"Wrong! You have {attempts} attempts left.\n")
+                    else:  # No attempts left
+                        print(f"Wrong! The correct translations are: {', '.join(answers)}\n")
 
 
 def main():
@@ -252,7 +288,7 @@ def main():
     - Prompts the user for a username.
     - Manages user's progress file based on the username.
     - Prompts the user to select the translation direction (English to target language or vice versa).
-    - Loads the dictionary file containing word pairs.
+    - Loads the dictionary file containing word/sentence pairs.
     - Shuffles the dictionary.
     - Calls the play_game function to start the game, passing in the dictionary and user's choice.
     - Exit is handled by typing 'q' within the play_game function.
